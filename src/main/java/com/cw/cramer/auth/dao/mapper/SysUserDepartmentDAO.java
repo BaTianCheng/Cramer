50c99cfd62f6ba79961dbc3cab6e8be51d512603
wicks <wicks@LAPTOP-OL8IHH62>
2017-04-26 15:11:02 +0800

    修改

---
+++ src/main/java/com/cw/cramer/auth/dao/mapper/SysUserDepartmentDAO.java
@@ -0,0 +1,22 @@
+package com.cw.cramer.auth.dao.mapper;
+
+import com.cw.cramer.auth.entity.SysUserDepartment;
+import com.cw.cramer.auth.entity.SysUserDepartmentExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface SysUserDepartmentDAO {
+    long countByExample(SysUserDepartmentExample example);
+
+    int deleteByExample(SysUserDepartmentExample example);
+
+    int insert(SysUserDepartment record);
+
+    int insertSelective(SysUserDepartment record);
+
+    List<SysUserDepartment> selectByExample(SysUserDepartmentExample example);
+
+    int updateByExampleSelective(@Param("record") SysUserDepartment record, @Param("example") SysUserDepartmentExample example);
+
+    int updateByExample(@Param("record") SysUserDepartment record, @Param("example") SysUserDepartmentExample example);
+}
---
+++ src/main/java/com/cw/cramer/auth/entity/SysUserDepartment.java
@@ -0,0 +1,23 @@
+package com.cw.cramer.auth.entity;
+
+public class SysUserDepartment {
+    private Integer userId;
+
+    private Integer departmentId;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Integer departmentId) {
+        this.departmentId = departmentId;
+    }
+}
